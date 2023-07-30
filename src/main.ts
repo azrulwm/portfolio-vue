ce8143207434ed88abcb35505ea00c39475b7286
azrulwm <azrulazmi.wm@gmail.com>
2023-07-30 23:23:24 +0800

    Fixing error issues with typescript

--- src/main.ts
+++ src/main.ts
@@ -1,25 +1,22 @@
-import { createApp } from 'vue';
-import App from './App.vue';
-import router from './router';
-import './assets/css/app.css';
-import BackToTop from 'vue-backtotop';
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
+import "./assets/css/app.css";
 
-const feather = require('feather-icons');
+const feather = require("feather-icons");
 feather.replace();
 
-createApp(App)
-	.use(router)
-	.use(BackToTop)
-	.mount('#app');
+createApp(App).use(router).mount("#app");
 
-const appTheme = localStorage.getItem('theme');
+const appTheme = localStorage.getItem("theme");
 
 // Check what is the active theme and change theme when user clicks on the theme button in header.
-if (
-	appTheme === 'dark' &&
-	document.querySelector('body').classList.contains('app-theme')
-) {
-	document.querySelector('body').classList.add('bg-primary-dark');
-} else {
-	document.querySelector('body').classList.add('bg-secondary-light');
+const body = document.querySelector("body");
+
+if (body !== null) {
+  if (appTheme === "dark" && body.classList.contains("app-theme")) {
+    body.classList.add("bg-primary-dark");
+  } else {
+    body.classList.add("bg-secondary-light");
+  }
 }
